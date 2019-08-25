890ba77f7a0ae46dd223a3359f61c9610115da5c
mrtwistyneck <wattskimzey@gmail.com>
2019-08-24 23:18:55 -0500

    created loadquestion function and displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,13 @@ let currentQuestion = 0;
 let score = 0;
 let lost = 0;
 let timer;
+
+function loadQuestion() {
+
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].choices;
+
+    $('#game').html('<h4>' + question + '<h4>');
+}
+
+loadQuestion();

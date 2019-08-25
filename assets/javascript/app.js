17e39c46c4644ee8156e7d689b248afb3e9232ae
mrtwistyneck <wattskimzey@gmail.com>
2019-08-24 23:37:14 -0500

    added load choices function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-let counter = 30;
+let counter = 5;
 let currentQuestion = 0;
 let score = 0;
 let lost = 0;
@@ -9,10 +9,19 @@ function loadQuestion() {
     const question = quizQuestions[currentQuestion].question;
     const choices = quizQuestions[currentQuestion].choices;
 
-    $('#time').html(counter);
+    $('#time').html("This is how many seconds you have left, oh Knower of Little Trivia... " + counter);
     $('#game').html(`
         <h4>${question}</h4>
+        ${loadChoices(choices)}
     `);
 }
 
+function loadChoices(choices) {
+    let result = '';
+
+    for (let i = 0; i < choices.length; i++ ){
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }
+    return result;
+}
 loadQuestion();

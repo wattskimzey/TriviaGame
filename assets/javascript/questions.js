eee8b7ca0f0c72bdbf1b22bae2d0b0535b255873
mrtwistyneck <wattskimzey@gmail.com>
2019-08-24 22:44:50 -0500

    added test questions to questions.js

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,1 +1,17 @@
-console.log("question");
+const quizQuestions = [
+    {
+        question: "What was your great-great uncle's best friend's worst enemy's favorite cat?",
+        choices: ["Garfield", "Robo-Garfield", "The Ghost of Garfield", "A dog with a cat costume", "trick question, his enemy hated all cats!"],
+        correctAnswer: "trick question, his enemy hated all cats!"
+    },
+    {
+        question: "Why do cats do that funny thing with their mouth open?",
+        choices: ["IDK", "to eat your soul", "because they can haz cheeseburger", "they ate a cruncy frog", "cat brain freeze"],
+        correctAnswer: "cat brain freeze"
+    },
+    {
+        question: "In what way can you make a cat's litterbox less stinky?",
+        choices: ["get more cats", "buy a dog", "you know what dogs like to eat, don't you?", "dogs like to eat MMMPPPHH MPPPHHH", "we're sorry, these answers have grown disgusting. this is the correct answer"],
+        correctAnswer: "we're sorry, these answers have grown disgusting. this is the correct answer"
+    },
+];
